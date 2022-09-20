9a322b21fffb97a39b72c1a999782fecbb3c6e7f
Kevin Kraiss <kevinkraiss@gmail.com>
2022-09-19 21:46:05 -0500

    keydown listener, finish render characters function'

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,21 +2,38 @@
 var startBtn = document.querySelector('.start-button')
 var wordBlanksEl = document.querySelector('.word-blanks')
 
+var validChars = 'abcdefghijklmnopqrstuvwxyz'
 var words = ['html', 'function', 'query', 'localstorage', 'timeout', 'intervals', 'javascript', 'terminal']
 var word
+var guessedCharacters = []
 // score variable
 // timeleft variable
+
+function handleKeydown(event) {
+    console.log(event.key)
+    if (validChars.includes(event.key)) {
+        // keep track of character guessed
+        guessedCharacters.push(event.key)
+        //rerender wordBlanks.textContent
+        renderCharacters()
+    }
+    // validate key
+}
 
 // reset the word-blanks, an _ for each letter of new word
 function renderCharacters() {
     var str = ""
     for (var i = 0; i < word.length; i++) {
+        var letter = word[i]
         // if we guess the right character
+        if (guessedCharacters.includes(letter)) {
+            str += letter + ' '
             // push into string
+        } else {
         //else
             //push an _ into str
             str += '_ '
-            console.log(str)
+        }
     }
     // set textContent of wordBlanksEl to be str
     wordBlanksEl.textContent = str.trim()
@@ -31,4 +48,4 @@ renderCharacters()
 
 startBtn.addEventListener('click', startRound)
 
-
+document.addEventListener('keydown', handleKeydown)
